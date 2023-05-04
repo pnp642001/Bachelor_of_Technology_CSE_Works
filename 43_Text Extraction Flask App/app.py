b0f8dee9e985003efbc71be330f25f6c7abd0bd9
Parth Niteshkumar Patel <parth.642001@gmail.com>
2023-05-04 15:04:39 +0530

    Text Extraction Project Commit

---
+++ 43_Text Extraction Flask App/app.py
@@ -0,0 +1,51 @@
+#PROJECT BY: PARTH NITESHKUMAR PATEL
+#ID: I-003
+#VERIFICATION STATUS: VERIFIED BY THE TEAM LEADER(E-045)
+#ACCESS LEVEL: SHOW ACCESS ALLOWED
+#POST VERIFICATION MODIFICATION ACCESS: DENIED
+
+'''
+ADDITIONAL COMMENTS:
+1.The development of HTML forms falls outside the scope of I-003's responsibilities, as this is the responsibility of the development team.
+
+2.All coding related to Flask has been completed by the developers and is not within the scope of I-003's duties.
+
+3.To uphold the confidentiality of our client and organization,certain lines of code were omitted from I-003's project during verification.
+As the project was developed for financial purposes, this was done in accordance with our organization's policies.
+It is important to note that I-003 is now prohibited from making any modifications to the verified code.
+'''
+#
+# PROJECT DETAILS:
+#
+# NAME: TEXT EXTRACTION PROJECT
+#
+# THIS PROJECT IS A DEMONSTRATION OF THE TECHNIQUES USED BY ME DURING THE INTERNSHIP
+#
+# IN THIS PROJECT, WE WILL EXTRACT THE TEXT FROM THE IMAGES AND THEN, WE WILL
+
+# PROJECT FLOW:
+#
+# 1. DOWNLOAD AND INSTALL THE TESSERACT LIBRARY ALONG WITH WITH THE DEPENDENCIES:
+#    NOTE: THIS STEP NEEDS TO BE PERFORMED ONLY ONE TIME, IF DONE IN A LOCAL IDE, HOWEVER, ON  COLAB IT IS REQUIRED EVERYIME
+#
+# 2. LOADING THE IMAGE
+#
+# 3. RESIZING THE IMAGE
+#
+# 4. USE PYTESSERACT FOR THE EXTRACTION
+#
+# 5. USE OF COMPUTER VISION FOR FURTHER PROCESSING
+#
+# 6. REMOVE THE NOISE FROM THE IMAGE
+#
+# 7. PERFORM THE THRESHOLD TRANSFORMATION
+#
+# 8. USING THE ERODE FUNCTION OF CV2
+#
+# 9. DRAW RECTANGLES AROUND THE PARTICULAR WORDS
+
+
+from app import app
+
+if __name__ == '__main__':
+   app.run()
